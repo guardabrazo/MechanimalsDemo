e13209149e4ed278afd6644494964f4d805d9a24
guardabrazo <guardabrazo@MacBook-Pro-de-Federico.local>
2014-07-18 18:52:18 +0200

    Initial Commit

---
+++ Mechanimals/GBZAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  GBZAppDelegate.h
+//  Mechanimals
+//
+//  Created by Federico Guardabrazo Vallejo on 18/07/14.
+//  Copyright (c) 2014 guardabrazo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GBZAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Mechanimals/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Mechanimals
+//
+//  Created by Federico Guardabrazo Vallejo on 18/07/14.
+//  Copyright (c) 2014 guardabrazo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "GBZAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([GBZAppDelegate class]));
+    }
+}
