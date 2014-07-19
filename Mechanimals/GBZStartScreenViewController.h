54e4c2ad4e8800f81b104b5ad3e6157e1ce5f420
guardabrazo <guardabrazo@MacBook-Pro-de-Federico.local>
2014-07-19 13:56:51 +0200

    Created StartScreenViewController

---
+++ Mechanimals/GBZStartScreenViewController.h
@@ -0,0 +1,13 @@
+//
+//  GBZStartScreenViewController.h
+//  Mechanimals
+//
+//  Created by Federico Guardabrazo Vallejo on 19/07/14.
+//  Copyright (c) 2014 guardabrazo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GBZStartScreenViewController : UIViewController
+
+@end
---
+++ Mechanimals/GBZStartScreenViewController.m
@@ -0,0 +1,38 @@
+//
+//  GBZStartScreenViewController.m
+//  Mechanimals
+//
+//  Created by Federico Guardabrazo Vallejo on 19/07/14.
+//  Copyright (c) 2014 guardabrazo. All rights reserved.
+//
+
+#import "GBZStartScreenViewController.h"
+
+@interface GBZStartScreenViewController ()
+
+@end
+
+@implementation GBZStartScreenViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
